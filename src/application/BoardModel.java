ecc88fa202000734a9d9221cdb384dfd8d392b60
Jennifer Nghi Nguyen <jennifernghi@yahoo.com>
2016-10-29 22:58:45 -0700

    use Integer[] represent board data instead of Chip[]

--- src/application/BoardModel.java
+++ src/application/BoardModel.java
@@ -2,18 +2,38 @@ package application;
 
 public class BoardModel {
 	
-	private Chip[][] boardArray; 
+	private Integer[][] boardArray; 
 	private int winningMove; 
+	private int row;
+	
+	public void setRow(int row){
+		this.row= row;
+	}
+	public int getRow(){
+		return this.row;
+	}
 	
 	public BoardModel(int size, int winningMove)
 	{
-		boardArray = new Chip[size][size]; //no need for setters because it will break encapsulation
+		boardArray = new Integer[size][size]; //no need for setters because it will break encapsulation
+		for(int i=0; i<boardArray.length;i++){
+			for(int j = 0; j< boardArray[i].length; j++){
+				boardArray[i][j] = 0;
+				
+			}
+		}
 		this.winningMove = winningMove; 
 	}
 	
 	void putChip(Chip chip, int column)
 	{
-
+		boardArray[getRow()][column] = chip.getValue();
+		for(int i=0; i<boardArray.length;i++){
+			for(int j = 0; j< boardArray[i].length; j++){
+				System.out.println("row " +i + "col " + j +" :  " +boardArray[i][j]);
+				
+			}
+		}
 	}
 	
 	int checkWinner()
