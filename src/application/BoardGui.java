af88568a2983d80a81ca82bbb85c450b22141d29
Jennifer Nghi Nguyen <jennifernghi@yahoo.com>
2016-10-29 22:57:44 -0700

    add color to the grid
    add color to the grid and update boardArray data

--- src/application/BoardGui.java
+++ src/application/BoardGui.java
@@ -2,10 +2,9 @@ package application;
 
 
 
-//import com.sun.prism.paint.Color;
-
 import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -17,9 +16,8 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
-
 import javafx.stage.Stage;
 
 
@@ -31,15 +29,22 @@ public class BoardGui extends Application{
 	private GridPane grid;
 	private Button player1Button;
 	private Button player2Button;
-	
+	private double chipWidth =50;
+	private double chipHeight =50;
 	public BoardGui(BoardPresenter presenter, int size) {
 		//System.out.println("board gui");
 		this.presenter = presenter; 
 		presenter.setView(this);
 		
 		createGrid(size);
+		
 	}
 	
+	@Override
+	public void init() throws Exception {
+		//this.chipHeight=50;
+		//this.chipWidth=50;
+	}
 	public void setGridPane(GridPane grid) {
 		this.grid = grid;
 	}
@@ -63,6 +68,12 @@ public class BoardGui extends Application{
 	}
 	public void setPlayer2Button(Button player2Button) {
 		this.player2Button = player2Button;
+	}
+	public double getChipWidth() {
+		return this.chipWidth;
+	}
+	public double getChipHeight() {
+		return this.chipHeight;
 	}
 	
 	public VBox showRightBoxGUI(){
@@ -96,6 +107,11 @@ public class BoardGui extends Application{
 		alert.showAndWait();
 	}
 	
+	public Rectangle rectangleChip(Paint color){
+		Rectangle rec = new Rectangle(getChipWidth()-1, getChipHeight()-1);
+		rec.setFill(color);
+		return rec;
+	}
 	public void createGrid(int size){
 		
 		grid = new GridPane();
@@ -103,13 +119,13 @@ public class BoardGui extends Application{
 		
 		//add colums on the grid using numberOfColumns
         for(int i = 0; i < size; i++) {
-            ColumnConstraints column = new ColumnConstraints(50);
+            ColumnConstraints column = new ColumnConstraints(getChipHeight());
             grid.getColumnConstraints().add(column);
             
         }
         //add rows on the grid using numberOfRow
         for(int i = 0; i < size; i++) {
-            RowConstraints row = new RowConstraints(50);
+            RowConstraints row = new RowConstraints(getChipWidth());
             grid.getRowConstraints().add(row);
         }
      
@@ -119,12 +135,33 @@ public class BoardGui extends Application{
             for (int j = 0; j < size; j++) {
                 Pane pane = new Pane();
                 int column= i;
+                int row = j;
           
                 //when the cell is clicked, add a rectangle with chosen color onto the cell
                 pane.setOnMouseReleased(e -> {
-               	
-                 presenter.putChip(column);
-                    
+                	
+                	presenter.setRow(row);
+                 //show whose is playing using button
+                 if(presenter.getTurn()==1){
+                	 System.out.println("turn: "+presenter.getTurn());
+                	 System.out.println("player 1: "+presenter.getPlayer1().getChip().getColor());
+                	
+                	 player1Button.setDisable(false);
+                	 player2Button.setDisable(true);
+                	 pane.getChildren().add(rectangleChip(presenter.getPlayer1().getChip().getColor()));
+                	 presenter.putChip(column);
+                	 pane.setDisable(true);
+                 }else{
+                	 System.out.println("turn: "+presenter.getTurn());
+                	 System.out.println("player 2: "+presenter.getPlayer2().getChip().getColor());
+                	 
+                	 player1Button.setDisable(true);
+                	 player2Button.setDisable(false);
+                	 pane.getChildren().add(rectangleChip(presenter.getPlayer2().getChip().getColor()));
+                	 presenter.putChip(column);
+                	 pane.setDisable(true);
+                 }
+                   
                 });
                 
                 
@@ -154,7 +191,7 @@ public class BoardGui extends Application{
 		
 		Scene scene = new Scene(root, 700,700);
 		//link to the css file
-		scene.getStylesheets().add(this.getClass().getResource("game.css").toExternalForm());
+		scene.getStylesheets().add(this.getClass().getResource("/game.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Connect 4");
 		primaryStage.show();
