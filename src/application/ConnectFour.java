9aad1dce2b2dda99210f844dd066d69645b85402
Jennifer Nghi Nguyen <jennifernghi@yahoo.com>
2016-11-02 23:05:51 -0700

    check integer inputs

--- src/application/ConnectFour.java
+++ src/application/ConnectFour.java
@@ -9,15 +9,23 @@ public class ConnectFour extends Application{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try{
-			
-		
-			arguments[0] = Integer.parseInt(args[0].trim());
-			arguments[1] = Integer.parseInt(args[1].trim());
-			if(arguments[1]<arguments[0]){
-				launch(args);
-			}else
+			/**
+		 * since every character has a corresponding decimal value
+		 * 0 to 9 have corresponding int values from 48 to 57
+		 */
+			if((args[0].trim().charAt(0)>= 48 && args[0].trim().charAt(0) <=57) && (args[1].trim().charAt(0)>= 48 && args[1].trim().charAt(0) <=57))
 			{
-				System.out.println("winner move must smaller than size");
+				arguments[0] = Integer.parseInt(args[0].trim());
+				arguments[1] = Integer.parseInt(args[1].trim());
+				if(arguments[1]<arguments[0]){
+					launch(args);
+				}else
+				{
+					System.out.println("winner move must smaller than size");
+					System.exit(0);
+				}
+			}else{
+				System.out.println("size and winning move must be an integer");
 				System.exit(0);
 			}
 				
