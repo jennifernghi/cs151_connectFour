1b083b8f0474e9d3930de5abb47915e7493fe738
Jennifer Nghi Nguyen <jennifernghi@yahoo.com>
2016-11-03 12:43:30 -0700

    update comments

--- src/application/ConnectFour.java
+++ src/application/ConnectFour.java
@@ -1,8 +1,12 @@
-package application;
+
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+/**
+ * entry point to run the game
+ *
+ *
+ */
 public class ConnectFour extends Application {
 
 	private static Integer[] arguments = new Integer[2];
@@ -10,29 +14,25 @@ public class ConnectFour extends Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			/**
-			 * since every character has a corresponding decimal value 0 to 9
-			 * have corresponding int values from 48 to 57
-			 */
-			if ((args[0].trim().charAt(0) >= 48 && args[0].trim().charAt(0) <= 57)
-					&& (args[1].trim().charAt(0) >= 48 && args[1].trim().charAt(0) <= 57)) {
+				//get args[0] and args[1] and add them into arguments[] after parse them into Integer
+			
 				arguments[0] = Integer.parseInt(args[0].trim());
 				arguments[1] = Integer.parseInt(args[1].trim());
+				//if winningmove < size
 				if (arguments[1] < arguments[0]) {
-					launch(args);
+					launch(args);//launch gui
 				} else {
+					//show error
 					System.out.println("winner move must smaller than size");
 					System.exit(0);
 				}
-			} else {
-				System.out.println("size and winning move must be an integer");
-				System.exit(0);
-			}
+			
 
 		} catch (Exception e) {
+			//show errors
 			System.out.println("Invalid argument type. check command line!");
-			System.out.println("java application.ConnectFour size winningmove");
-			System.out.println("or java -jar ConnectFour size winningmove");
+			System.out.println("java ConnectFour size winningmove");
+			System.out.println("or java -jar Connect4.jar size winningmove");
 			System.exit(0);
 		}
 
