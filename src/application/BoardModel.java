203ec58b94b51e7b64c18877de3c24c80dbd222e
Faith Chau <faith_chau@Faiths-MacBook-Pro.local>
2016-11-03 00:53:16 -0700

    merge issue resolved

--- src/application/BoardModel.java
+++ src/application/BoardModel.java
@@ -33,8 +33,8 @@ public class BoardModel {
 
 	public int checkWinner() {
 
-		//check if it is tie
-		if (numOfChips == spaces) 
+		// check if it is tie
+		if (numOfChips == spaces)
 			return -1;
 		else {
 			int consecutiveChipValue = 0;
@@ -48,13 +48,10 @@ public class BoardModel {
 			// check diagonal left - to - right
 
 			consecutiveChipValue = checkOnAndAboveDiagonalBottomRightToLeft(consecutiveChipValue, repeat);
-			
 			consecutiveChipValue = checkBelowDiagonalBottomRightToLeft(consecutiveChipValue, repeat);
-			// check diagonal right - to - left
-			
 			consecutiveChipValue = checkOnAndAboveDiagonalBottomLeftToRight(consecutiveChipValue, repeat);
-			
 			consecutiveChipValue = checkBelowDiagonalBottomLeftToRight(consecutiveChipValue, repeat);
+
 			return consecutiveChipValue;
 		}
 	}
@@ -104,13 +101,14 @@ public class BoardModel {
 		return consecutiveChipValue;
 
 	}
-	
+
 	private int checkOnAndAboveDiagonalBottomRightToLeft(int consecutiveChipValue, int repeat) {
 		for (int column = 0; column < boardArray.length; column++) {
 			for (int row = 1; row < boardArray.length; row++) {
 				// compare with previous diagonal value
-				if (row+ column < boardArray.length && boardArray[row][row+ column] != null && boardArray[row -1][(row + column) - 1] != null) {
-					if (boardArray[row][row+ column].getValue() == boardArray[row- 1][(row + column) - 1].getValue())
+				if (row + column < boardArray.length && boardArray[row][row + column] != null
+						&& boardArray[row - 1][(row + column) - 1] != null) {
+					if (boardArray[row][row + column].getValue() == boardArray[row - 1][(row + column) - 1].getValue())
 						++repeat;
 					else
 						repeat = 1;
@@ -126,15 +124,17 @@ public class BoardModel {
 			}
 		}
 		return consecutiveChipValue;
-		}
-		
+	}
 
 	private int checkBelowDiagonalBottomRightToLeft(int consecutiveChipValue, int repeat) {
 		for (int row = 0; row < boardArray.length; row++) {
 			for (int column = 1; column < boardArray.length; column++) {
 				// compare with previous column
-				if (row + column < boardArray.length && row+column != column && boardArray[row+column][column] != null && boardArray[((row + column)-1)][column- 1] != null) {
-					if (boardArray[row+column][column].getValue() == boardArray[((row + column)-1)][column- 1].getValue())
+				if (row + column < boardArray.length && row + column != column
+						&& boardArray[row + column][column] != null
+						&& boardArray[((row + column) - 1)][column - 1] != null) {
+					if (boardArray[row + column][column].getValue() == boardArray[((row + column) - 1)][column - 1]
+							.getValue())
 						++repeat;
 					else
 						repeat = 1;
@@ -143,20 +143,22 @@ public class BoardModel {
 					// winningMove.
 					if (repeat == winningMove) {
 						// Return color of the winner
-						consecutiveChipValue = boardArray[row+column][column].getValue();
+						consecutiveChipValue = boardArray[row + column][column].getValue();
 					}
 				} else
 					repeat = 1;
 			}
 		}
 		return consecutiveChipValue;
-		}
+	}
+
 	private int checkOnAndAboveDiagonalBottomLeftToRight(int consecutiveChipValue, int repeat) {
-		for (int column = boardArray.length-1; column > 0; column--) {
+		for (int column = boardArray.length - 1; column > 0; column--) {
 			for (int row = 1; row < boardArray.length; row++) {
 				// compare with previous diagonal value
-				if (column- row >= 0 && boardArray[row][column -row] != null && boardArray[row -1][(column-row) + 1] != null) {
-					if (boardArray[row][column -row].getValue() == boardArray[row -1][(column-row) + 1].getValue())
+				if (column - row >= 0 && boardArray[row][column - row] != null
+						&& boardArray[row - 1][(column - row) + 1] != null) {
+					if (boardArray[row][column - row].getValue() == boardArray[row - 1][(column - row) + 1].getValue())
 						++repeat;
 					else
 						repeat = 1;
@@ -165,29 +167,28 @@ public class BoardModel {
 					// winningMove.
 					if (repeat == winningMove) {
 						// Return color of the winner
-						consecutiveChipValue = boardArray[row][column -row].getValue();
+						consecutiveChipValue = boardArray[row][column - row].getValue();
 					}
 				} else
 					repeat = 1;
 			}
 		}
 		return consecutiveChipValue;
-		}
+	}
+
 	private int checkBelowDiagonalBottomLeftToRight(int consecutiveChipValue, int repeat) {
-		for (int row =1; row <=boardArray.length-2; row++) {
-			for (int column = boardArray.length-2, i = row +1; column >= 0 && i <=boardArray.length-1; column--, i++) {
-				if (boardArray[i][column] != null  && i -1 >=0 && column +1 <boardArray.length && boardArray[i-1][column+1] !=null) {
-					
-<<<<<<< HEAD
-					if (boardArray[i][column].getValue() == boardArray[row][pivot].getValue())
-=======
-					if (boardArray[i][column].getValue() == boardArray[i-1][column+1].getValue())
-					{
-						
->>>>>>> bed04566016816b0752def70687f4fe979f54bed
+		for (int row = 1; row <= boardArray.length - 2; row++) {
+			for (int column = boardArray.length - 2, i = row + 1; column >= 0
+					&& i <= boardArray.length - 1; column--, i++) {
+				if (boardArray[i][column] != null && i - 1 >= 0 && column + 1 < boardArray.length
+						&& boardArray[i - 1][column + 1] != null) {
+					if (boardArray[i][column].getValue() == boardArray[i - 1][column + 1].getValue()) {
+
 						++repeat;
-					else
+
+					} else {
 						repeat = 1;
+					}
 					// Check if there are consecutive chip in a row that =
 					// winningMove.
 					if (repeat == winningMove) {
@@ -199,13 +200,13 @@ public class BoardModel {
 			}
 		}
 		return consecutiveChipValue;
-		}
-	public void clear()
-	{
+	}
+
+	public void clear() {
 		numOfChips = 0;
-		
-		for (int i =0; i<boardArray.length;i++)
-			for (int j =0; j<boardArray.length;j++)
-				boardArray[i][j] = null; 
+
+		for (int i = 0; i < boardArray.length; i++)
+			for (int j = 0; j < boardArray.length; j++)
+				boardArray[i][j] = null;
 	}
 }
